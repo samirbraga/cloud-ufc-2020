bc87fc0ba31f9a726e3c65d7c72dafd1f2f9273a
tfraxe <tfraxe@gmail.com>
2020-04-03 17:27:19 -0300

    Adição do config.js

---
+++ server/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    JWT_SECRET: process.env.JWT_SECRET,
+    DB_PASS: process.env.DB_PASS,
+    DB_USER: process.env.DB_USER,
+    DB_NAME: process.env.DB_NAME,
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    BCRYPT_SALT_ROUNDS: 10,
+    MAX_SIGNUP_ATTEMPTS: 5,
+    PAGE_SIZE: 10
+};
