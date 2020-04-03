671b7a67b0ccb8d8665d22cdd23fc2abcff8a8bc
tfraxe <tfraxe@gmail.com>
2020-04-03 19:04:10 -0300

    Adição do primeiro service - user

---
+++ server/service/user.js
@@ -0,0 +1,419 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const path = require('path');
+const responses = require('./utils/responses')['pt-br'];
+const handles = require('./utils/handles');
+const paginate = require('./utils/paginate');
+const config = require('../config');
+const dbUtils = require('../db/utils');
+const TokenBlacklist = require('../model/TokenBlacklist');
+const Users = require('../model/Users');
+
+const _generateVerificationCode = () => {
+    const rand = () => Math.floor(Math.random() * 10); 
+    return `${rand()}${rand()}${rand()}${rand()}`;
+};
+
+module.exports = {
+    get(req, res) {
+        const providedUserId = req.params.user_id;
+        if (providedUserId) {
+            const tokenUserId = req.tokenData.user_id;
+
+            if (providedUserId === tokenUserId) {
+                Users.findOne({
+                    where: {
+                        id: providedUserId,
+                        confirmed: true
+                    },
+                    attributes: {
+                        exclude: ['password']
+                    },
+                    include: [
+                    ]
+                })
+                .then(user => {
+                    if (!user) {
+                        return res.status(404).json({
+                            status: 'error',
+                            ...responses.errors.noUserFound
+                        });
+                    }
+                })
+                .catch(handles.handleServerError(res));
+            } else {
+                res.status(403).json({
+                    status: 'error',
+                    ...responses.errors.accessDenied
+                });
+            }
+        } else {
+            Users.findAndCountAll({
+                where: {
+                    confirmed: true
+                },
+                attributes: {
+                    exclude: ['password']
+                },
+                include: [
+                ],
+                ...paginate(req).query
+            })
+            .then(result => {
+                res.json({
+                    ...paginate(req).response(result),
+                    status: 'success',
+                    users: result.rows
+                });
+            })
+            .catch(handles.handleServerError(res));
+        }
+    },
+    get_by_username(req, res) {
+        const providedUsername = req.query.q;
+        Users.findOne({
+            where: {
+                id: providedUsername
+            },
+            attributes: {
+                exclude: ['password']
+            },
+            include: [
+            ]
+        })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({
+                    status: 'error',
+                    ...responses.errors.noUserFound
+                });
+            }
+        })
+        .catch(handles.handleServerError(res));
+    },
+    
+    create(req, res) {
+        bcrypt.hash(req.body.password, config.BCRYPT_SALT_ROUNDS, (err, hash) => {
+            if (err)
+                return handles.handleServerError(res)();
+            
+            Users.create({
+                ...req.body,
+                password: hash
+            })
+            .then(user => {
+                const code = _generateVerificationCode();
+                const tokenData = {
+                    id: dbUtils.generateId(),
+                    user_id: user.id.toString(),
+                };
+
+                jwt.sign(tokenData, config.JWT_SECRET, { expiresIn: '24h' }, (err, token) => {
+                    if (err)
+                        return handles.handleServerError(res)();
+
+                    TokenBlacklist.create({
+                        id: tokenData.id,
+                        token,
+                        type: 'signup',
+                        code,
+                        attempts: 0
+                    })
+                    .then(() => {
+                        if (process.env.NODE_ENV === 'development')
+                            console.log('VERIFICATION CODE: ', code);
+
+                        EmailService.sendActivationEmail(user, code);
+
+                        res.json({
+                            status: 'success',
+                            message: "Foi enviado um código de verificação para o seu email. Você tem até 24 horas para ativar sua conta.",
+                            signup_token: token
+                        });
+                    })
+                    .catch(handles.handleServerError(res));
+                });
+            })
+            .catch(err => {
+                if (err.errors) {
+                    const emailAlreadyExists = err.errors.some(error => {
+                        return error.validatorKey === 'not_unique' && error.path === 'email';
+                    });
+    
+                    if (emailAlreadyExists) {
+                        return res.status(403).json({
+                            status: 'error',
+                            ...responses.errors.emailAlreadyUsed
+                        });
+                    }
+                }
+
+                handles.handleServerError(res)(err);
+            });
+        });
+    },
+    resendCode(req, res) {
+        Users.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        .then(user => {
+            if (!user) {
+                return res.status(403).json({
+                    status: 'error',
+                    ...responses.errors.authenticationFailed
+                });
+            }
+
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                if (err)
+                    return handles.handleServerError(res)();
+
+                if (result === true) {
+                    const code = _generateVerificationCode();
+                    const tokenData = {
+                        id: dbUtils.generateId(),
+                        code,
+                        user_id: user.id.toString()
+                    };
+
+                    jwt.sign(tokenData, config.JWT_SECRET, { expiresIn: '24h' }, (err, token) => {
+                        if (err) {
+                            return handles.handleServerError(res)();
+                        }
+
+                        TokenBlacklist.create({
+                            id: tokenData.id,
+                            token,
+                            type: 'signup',
+                            attempts: 0
+                        })
+                        .then(() => {
+                            if (process.env.NODE_ENV === 'development')
+                                console.log('VERIFICATION CODE: ', code);
+
+                            EmailService.sendActivationEmail(user, code);
+
+                            res.json({
+                                status: 'success',
+                                message: "Foi enviado um código de verificação para o seu email. Você tem até 24 horas para ativar sua conta.",
+                                signup_token: token
+                            });
+                        })
+                        .catch(handles.handleServerError(res));
+                    });
+                } else {
+                    res.status(403).json({
+                        status: 'error',
+                        ...responses.errors.authenticationFailed
+                    });
+                }
+            });
+        })
+        .catch(handles.handleServerError(res));
+    },
+    recoverPassword(req, res) {
+        const email = req.body.email;
+
+        Users.findOne({
+            where: {
+                email
+            }
+        })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({
+                    status: 'error',
+                    ...responses.errors.noUserFound
+                });
+            }
+
+            const tokenData = {
+                id: dbUtils.generateId(),
+                user_id: user.id.toString(),
+            };
+
+            jwt.sign(tokenData, config.JWT_SECRET, { expiresIn: '24h' }, (err, token) => {
+                if (err) {
+                    return handles.handleServerError(res)();
+                }
+
+                TokenBlacklist.create({
+                    id: tokenData.id,
+                    token,
+                    type: 'recover',
+                    attempts: 0
+                })
+                .then(() => {
+                    const recoveryLink = `http://${process.env.HOST}/api/user/recover-password?token=${token}`;
+                    if (process.env.NODE_ENV === 'development')
+                        console.log('RECOVER LINK: ', recoveryLink);
+
+                    EmailService.sendPasswordRecoveryEmail(user, recoveryLink);
+
+                    res.json({
+                        status: 'success',
+                        message: "Foi enviado um link de recuperação de senha para o seu email. Você tem até 24 horas para recuperar sua senha."
+                    });
+                })
+                .catch(handles.handleServerError(res));
+            });
+        })
+        .catch(handles.handleServerError(res));
+    },
+    recoverPasswordPage(req, res) {
+        res.sendFile(path.resolve(__dirname, 'views', 'recover-password.html'));
+    },
+    updatePassword(req, res) {
+        Users.findOne({
+            where: {
+                id: req.tokenData.user_id
+            }
+        })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({
+                    status: 'error',
+                    ...responses.errors.noUserFound
+                });
+            }
+
+            bcrypt.hash(req.body.new_password, config.BCRYPT_SALT_ROUNDS, (err, hash) => {
+                if (err) {
+                    return handles.handleServerError(res)();
+                }
+
+                user.set('password', hash).save()
+                .then(() => {
+                    res.json({
+                        status: 'success',
+                        user_id: req.tokenData.user_id
+                    });
+                })
+                .catch(handles.handleServerError(res));
+            });
+        })
+        .catch(handles.handleServerError(res));
+    },
+    login(req, res) {
+        Users.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        .then(user => {
+            if (!user) {
+                return res.status(403).json({
+                    status: 'error',
+                    ...responses.errors.authenticationFailed
+                });
+            }
+
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                if (err)
+                    return handles.handleServerError(res)();
+                
+                if (result === true) {
+                    const tokenData = {
+                        id: dbUtils.generateId(),
+                        user_id: user.id.toString()
+                    };
+
+                    jwt.sign(tokenData, config.JWT_SECRET, { expiresIn: '30d' }, (err, token) => {
+                        if (err)
+                            return handles.handleServerError(res)();
+
+                        TokenBlacklist.create({
+                            id: tokenData.id,
+                            token,
+                            type: 'access'
+                        })
+                        .then(() => {
+                            res.json({
+                                status: 'success',
+                                user_id: user.id,
+                                access_token: token
+                            });
+                        })
+                        .catch(handles.handleServerError(res));
+                    });
+                } else {
+                    res.status(403).json({
+                        status: 'error',
+                        ...responses.errors.authenticationFailed
+                    });
+                }
+            });
+        })
+        .catch(handles.handleServerError(res));
+    },
+    logout(req, res) {
+        TokenBlacklist.destroy({
+            where: {
+                id: req.tokenData.id
+            }
+        })
+        .then(() => {
+            res.json({
+                status: 'success',
+                message: 'Usuário saiu da sessão.'
+            });
+        })
+        .catch(handles.handleServerError(res));
+    },
+    update(req, res) {
+        const userId = req.params.user_id;
+
+        if (userId === req.tokenData.user_id) {
+            Users.update({
+                ...req.body
+            }, {
+                where: {
+                    id: userId
+                }
+            })
+            .then(([affected]) => {
+                if (affected === 0) {
+                    return res.status(404).json({
+                        status: 'error',
+                        ...responses.errors.noUserFound
+                    });
+                }
+    
+                res.json({
+                    status: 'success',
+                    user_id: userId
+                });
+            })
+            .catch(handles.handleServerError(res));
+        } else {
+            res.status(403).json({
+                status: 'error',
+                ...responses.errors.accessDenied
+            });
+        }
+    },
+    destroy(req, res) {
+        const userId = req.params.user_id;
+        Users.destroy({
+            where: {
+                id: userId
+            }
+        })
+        .then(destroyed => {
+            if (destroyed === 0) {
+                return res.status(404).json({
+                    status: 'error',
+                    ...responses.errors.noUserFound
+                });
+            }
+
+            res.json({
+                status: 'success',
+                user_id: userId
+            });
+        })
+        .catch(handles.handleServerError(res));
+    }
+};
