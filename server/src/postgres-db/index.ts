4204aba3a8c8d67c841614db246d6012a704d3d8
Samir Braga <samirchavess@gmail.com>
2020-04-25 16:40:51 -0300

    Corrigindo conexão com o banco

--- server/src/postgres-db/index.ts
+++ server/src/postgres-db/index.ts
@@ -4,10 +4,9 @@ import config from '../config'
 const sequelize = new Sequelize(config.DB_NAME, config.DB_USER, config.DB_PASS, {
     dialect: 'postgres',
     quoteIdentifiers: false,
-    host: `/cloudsql/${process.env.GC_DB_ID}`,
-    port: parseInt(config.DB_PORT),
+    host: `/cloudsql/{instance}`,
     dialectOptions: {
-        socketPath: `/cloudsql/${process.env.GC_DB_ID}`
+        socketPath: `/cloudsql/{instance}`
     },
     define: {
         timestamps: false,
