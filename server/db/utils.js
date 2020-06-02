e3e63841d3925f72709dc31290a718b180a2a70c
tfraxe <tfraxe@gmail.com>
2020-06-02 01:00:50 -0300

    Remoção de arquivos desnecessários

--- server/db/utils.js
+++
@@ -1,8 +0,0 @@
-const generateId = (max = 1e9, offset = 1000) => Math.floor(Math.random() * (max - offset)) + offset;
-
-generateId.smallint = () => generateId(32767, 100);
-
-module.exports = {
-    generateId
-};
-
--- webapp/mock/users.ts
+++
@@ -1,7 +0,0 @@
-import userData from './usersData.json';
-
-export default {
-    'GET /api/users': (req, res) => {
-        res.json(userData);
-    }
-};
